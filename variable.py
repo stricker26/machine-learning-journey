9e9d86150409201977847fc492d007320e32259d
Rey Jonas <rjhonnas@gmail.com>
2026-04-30 17:39:42 +0800

    Create variable.py
    Variables

---
+++ variable.py
@@ -0,0 +1,18 @@
+# Variables
+name = "Jonas"
+age = 38
+price = 3000
+is_active = True
+
+print("name:", name)
+print("age:", age)
+print("price:", price)
+print("is_active:", is_active)
+
+# List
+numbers = [1, 2, 3, 4, 5]
+print("First number:", numbers[0])
+
+# Loop
+for num in numbers:
+    print("Number:", num)
